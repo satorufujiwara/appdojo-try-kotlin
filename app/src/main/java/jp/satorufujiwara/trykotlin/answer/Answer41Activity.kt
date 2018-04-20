6ad2d00a52f1fda50178b70f834d7c3c1121c6a2
fujiwara-satoru <fujiwara_satoru@cyberagent.co.jp>
2018-04-20 11:30:28 +0900

    Answer4-1.

---
+++ app/src/main/java/jp/satorufujiwara/trykotlin/answer/Answer41Activity.kt
@@ -0,0 +1,23 @@
+package jp.satorufujiwara.trykotlin.answer
+
+import android.databinding.DataBindingUtil
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import jp.satorufujiwara.trykotlin.R
+import jp.satorufujiwara.trykotlin.databinding.Question4ActivityBinding
+
+class Answer41Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        DataBindingUtil
+                .setContentView<Question4ActivityBinding>(this, R.layout.question4_activity)
+                .run {
+                    recyclerView.layoutManager = LinearLayoutManager(this@Answer41Activity)
+                    recyclerView.adapter = Question4Adapter().apply {
+                        items.addAll((0 until 100).map { "No.$it" })
+                    }
+                }
+    }
+}
